453b7e1d571af45c2e7b961ede97f065704b2e3f
Vinicius Gonçalves <viniciuscgoncalves@hotmail.com>
2022-11-25 00:54:17 -0300

    projeto finalizado

--- src/resultado-busca/script.js
+++ src/resultado-busca/script.js
@@ -4,7 +4,7 @@ const { tipo, bairro } = getUrlVars()
 const filteredUsers = users.filter(user => user.tipo === tipo && user.endereco === bairro)
 
 filteredUsers.forEach(user => {
-    document.querySelector('.container').innerHTML += `
+    document.querySelector('.containerG').innerHTML += `
         <div class="usuario">
             <img src="${user.foto}" alt="">
             <h1>${user.nome}</h1>
